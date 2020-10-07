7502bdc88eb72d534a50780f6366058c61ba5656
YA <YA-androidapp@users.noreply.github.com>
2020-10-07 18:35:40 +0900

    Update main.py

--- main.py
+++ main.py
@@ -172,7 +172,7 @@ if __name__ == "__main__":
 
         # for cluster_count in range(2, K+1):
         # for cluster_count in [2,4,16,64]:
-        for cluster_count in [2,4,8,16,32,64,128.256,512,1024,2048,4096,8192,16384]:
+        for cluster_count in [2,4,8,16,32,64,128,256,512,1024,2048,4096,8192,16384]:
             clusters = fcluster(result, t=cluster_count, criterion='maxclust')
             for i, c in enumerate(clusters):
                 print(
